e678eb8fc4b52273c0f1827ab54255b56367ca9e
Adrian <github@ahards.com>
2022-10-04 12:41:17 +0100

    hide favicon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,47 +26,41 @@ const Nav = () => {
         initial="hidden"
         animate="show"
         >
-          <nav class="fade fadeOut text-xs  mt-8 mr-8">
+          <nav class="fade fadeOut text-xs mt-8 mr-8">
+
+            <div class="absolute hidden">
+                <span class="text-[#d2d8ff] text-lg font-bold"
+                  onClick={() => {
+                      window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+                    }
+                  }
+                >Adrian Hards</span>
+            </div>
+
             <div class="flex justify-end">
-
-
-
-              <motion.div variants={item}>
-                <div class="absolute left-48">
-                  <a href="#responsive-header" class="text-[#F7C58A] mr-9">
-                    <span class="text-[#d2d8ff] text-lg font-bold"
-                      onClick={() => {
-                          window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
-                        }
-                      }
-                    >Adrian Hards</span>
-                  </a>
-                </div>
-              </motion.div>
-
               <motion.div variants={item}>
                 <div class="text-[#F7C58A] mr-9">
                   <Link activeClass="active" smooth spy to="nav" >
-                    01. <span class="text-[#d2d8ff] hover:text-[#F7C58A]">About</span>
+                    01. <span class="text-[#d2d8ff] hover:text-[#F7C58A] link-underline">About</span>
                   </Link>
                 </div>
               </motion.div>
 
               <motion.div variants={item}>
                 <a href="#responsive-header" class="text-[#F7C58A] mr-9">
-                  02. <span class="text-[#d2d8ff] hover:text-[#F7C58A]">Experience</span>
+                  02. <span class="text-[#d2d8ff] hover:text-[#F7C58A] link-underline">Experience</span>
                 </a>
               </motion.div>
 
               <motion.div variants={item}>
                 <a href="#responsive-header" class="text-[#F7C58A] mr-9">
-                  03. <span class="text-[#d2d8ff] hover:text-[#F7C58A]">Work</span>
+                  03. <span class="text-[#d2d8ff] hover:text-[#F7C58A] link-underline">Work</span>
                 </a>
               </motion.div>
 
               <motion.div variants={item}>
                 <a href="#responsive-header" class="text-[#F7C58A] mr-9">
-                  04. <span class="text-[#d2d8ff] hover:text-[#F7C58A]">Contact</span>
+                  04. <span class="text-[#d2d8ff] hover:text-[#F7C58A] link-underline">Contact</span>
                 </a>
               </motion.div>
 
