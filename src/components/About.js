8236186c8d2dc698536a180e6fb97888e39180d8
Adrian <github@ahards.com>
2022-10-05 12:16:11 +0100

    reduce delay of fade in

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import { useAnimation, motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 
 const fadeIn = {
-  visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
   hidden: { opacity: 0, y: 50 }
 };
 
