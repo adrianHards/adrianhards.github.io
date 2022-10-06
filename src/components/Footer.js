9b6f34e006060cd429e3c54db602d79ad5a0b4e5
Adrian <github@ahards.com>
2022-10-06 12:30:19 +0100

    change link to github

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer class="">
       <span class="block text-sm text-[#D2D9FF] md:text-center mb-4">
-        <a href="https://github.com/adrianHards/adrian-hards" class="hover:text-[#F7C58A]">Built with React</a>
+        <a href="https://github.com/adrianHards/adrianhards.github.io" class="hover:text-[#F7C58A]">Built with React</a>
       </span>
     </footer>
   );
